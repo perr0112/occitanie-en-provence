3483711bffc21d222f7912679204a766d861f91d
perr0112 <clement.perrot@etudiant.univ-reims.fr>
2025-02-24 15:10:30 +0100

    chore: prepare build production

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,24 @@
-import restart from 'vite-plugin-restart'
+import restart from 'vite-plugin-restart';
 
 export default {
     root: 'src/', // Sources files (typically where index.html is)
     publicDir: '../static/', // Path from "root" to static assets (files that are served as they are)
-    server:
-    {
+    server: {
         host: true, // Open to local network and display URL
         open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env) // Open if it's not a CodeSandbox
     },
-    build:
-    {
+    build: {
         outDir: '../dist', // Output in the dist/ folder
         emptyOutDir: true, // Empty the folder first
-        sourcemap: true // Add sourcemap
+        sourcemap: true, // Add sourcemap
+        rollupOptions: {
+            input: {
+                main: 'src/index.html',
+                map: 'src/map.html'
+            }
+        }
     },
-    plugins:
-    [
-        restart({ restart: [ '../static/**', ] }) // Restart server on static file change
+    plugins: [
+        restart({ restart: ['../static/**'] }) // Restart server on static file change
     ],
-}
+};
